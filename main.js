9ae33a734953e462bcff2e427e56ad8f2aa3f8bd
Rafael Costa <rafargc@gmail.com>
2019-03-15 14:18:42 -0300

    funções onclick board e botão start prontos
    - esboço das primeiras funções de onclick para o board e o botão start.

---
+++ main.js
@@ -0,0 +1,49 @@
+//1-on click - tem qua aparecer a logo
+//2-tem que aparecer logos de forma alternada
+//3- depois que aparecer todos, tem que aparecer mensagem: seja de vitoria, perda ou sem vencedor
+//4- o botao de start muda ou criar outro para reiniciar o jogo
+
+//BOTÃO START
+function start (){
+    document.getElementById(1).innerHTML = '';
+    document.getElementById(2).innerHTML = '';
+    document.getElementById(3).innerHTML = '';
+    document.getElementById(4).innerHTML = '';
+    document.getElementById(5).innerHTML = '';
+    document.getElementById(6).innerHTML = '';
+    document.getElementById(7).innerHTML = '';
+    document.getElementById(8).innerHTML = '';
+    document.getElementById(9).innerHTML = '';
+}
+
+
+//OPÇAO 1 ON CLICK
+
+document.getElementById("1").addEventListener("click", function(){
+document.getElementById("1").innerHTML = "Hello World";
+});
+document.getElementById("2").addEventListener("click", function(){
+document.getElementById("2").innerHTML = "Hello World";
+});
+document.getElementById("3").addEventListener("click", function(){
+document.getElementById("3").innerHTML = "Hello World";
+});
+document.getElementById("4").addEventListener("click", function(){
+document.getElementById("4").innerHTML = "Hello World";
+});
+document.getElementById("5").addEventListener("click", function(){
+document.getElementById("5").innerHTML = "Hello World";
+});
+document.getElementById("6").addEventListener("click", function(){
+document.getElementById("6").innerHTML = "Hello World";
+});
+document.getElementById("7").addEventListener("click", function(){
+document.getElementById("7").innerHTML = "Hello World";
+});
+document.getElementById("8").addEventListener("click", function(){
+document.getElementById("8").innerHTML = "Hello World";
+});
+document.getElementById("9").addEventListener("click", function(){
+document.getElementById("9").innerHTML = "Hello World";
+});
+
